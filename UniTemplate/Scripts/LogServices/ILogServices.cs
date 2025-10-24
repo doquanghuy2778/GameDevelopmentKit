c412ba6431d42e8cb08f89da465cd3872d44da94
Do Quang Huy <huydq@admin123.com>
2025-10-24 14:06:33 +0700

    init submodule

---
+++ UniTemplate/Scripts/LogServices/ILogServices.cs
@@ -0,0 +1,19 @@
+﻿namespace UniTemplate.LogServices
+{
+    using UnityEngine;
+
+    public interface ILogServices
+    {
+        public void LogCallerInfor(int line, string memberName, string sourceFilePath);
+
+        public void LogError(string message);
+
+        public void Log(string message);
+
+        public void LogException(string message);
+
+        public void LogWarning(string message);
+
+        public void LogWithColor(string message, Color? color = null);
+    }
+}
