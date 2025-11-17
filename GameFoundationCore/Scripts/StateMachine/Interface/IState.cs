8e92f30ac35496750d9c5c418416ee85f4851508
Do Quang Huy <huydq@admin123.com>
2025-11-17 13:17:44 +0700

    add statemachine

---
+++ GameFoundationCore/Scripts/StateMachine/Interface/IState.cs
@@ -0,0 +1,13 @@
+﻿namespace GameDevelopmentKit.GameFoundationCore.StateMachine.Interface
+{
+    public interface IState
+    {
+        public void Enter();
+        public void Exit();
+    }
+
+    public interface IState<in TModel> : IState
+    {
+        public TModel Model { set; }
+    }
+}
