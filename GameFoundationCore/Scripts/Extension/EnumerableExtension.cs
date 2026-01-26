a193e96d5385968c987d624e5a7eedecbd189b68
doquanghuy2778 <dohuy0079@gmail.com>
2026-01-26 16:55:25 +0700

    add extension

---
+++ GameFoundationCore/Scripts/Extension/EnumerableExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace GameFoundationCore.Scripts.Extension
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class EnumerableExtension
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable) action(item);
+        }
+    }
+}
